b2ecf821fefcd08d86daa75e90a9192c25477ed1
xairaven <alexkovalevkpi@gmail.com>
2025-01-02 14:59:09 +0200

    (Feat) Started migrating wasm part to main branch

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@
 // Hide console window on Windows in release mode
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(not(target_arch = "wasm32"))]
 use crate::config::AppConfig;
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
     let app_config = AppConfig::from_env().unwrap_or_else(|err| {
         println!("Error: {err}");
@@ -28,9 +30,15 @@ fn main() {
     });
 }
 
-mod app;
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
 
-mod config;
+    ui::start();
+}
+
+mod app;
 mod context;
 mod fractals;
 mod graphics {
@@ -46,8 +54,12 @@ mod io {
     pub mod operations;
     pub mod screenshot;
 }
-mod logging;
 mod math {
     pub mod angle;
 }
 mod ui;
+
+#[cfg(not(target_arch = "wasm32"))]
+mod config;
+#[cfg(not(target_arch = "wasm32"))]
+mod logging;
--- src/ui.rs
+++ src/ui.rs
@@ -1,13 +1,13 @@
 use crate::app;
 use app::App;
-use eframe::NativeOptions;
 use egui::ThemePreference;
 
-pub const WINDOW_WIDTH: f32 = 900.0;
-pub const WINDOW_HEIGHT: f32 = 550.0;
-
+#[cfg(not(target_arch = "wasm32"))]
 pub fn start(crate_name: String, theme: ThemePreference) -> eframe::Result {
-    let native_options = NativeOptions {
+    const WINDOW_WIDTH: f32 = 900.0;
+    const WINDOW_HEIGHT: f32 = 550.0;
+
+    let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_title(&crate_name)
             .with_inner_size([WINDOW_WIDTH, WINDOW_HEIGHT])
@@ -35,6 +35,49 @@ pub fn start(crate_name: String, theme: ThemePreference) -> eframe::Result {
         native_options,
         Box::new(|cc| Ok(Box::new(App::new(cc, theme)))),
     )
+}
+
+#[cfg(target_arch = "wasm32")]
+pub fn start() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|cc| Ok(Box::new(App::new(cc, ThemePreference::Dark)))),
+            )
+            .await;
+
+        // Remove the loading text and spinner:
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                },
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                },
+            }
+        }
+    });
 }
 
 pub mod components {
